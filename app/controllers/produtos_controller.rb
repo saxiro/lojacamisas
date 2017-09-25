40afb696172de6f533751eae992c8f36686d4c77
saxiro <fabriciopbrb@gmail.com>
2017-09-25 14:23:06 -0300

    Aula 08 não finalizada

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,9 +1,15 @@
 class ProdutosController < ApplicationController
 
   def index
-    @produtosPorNome =  Produto.order(:nome).limit 5
+    @produtosPorNome =  Produto.order(:nome).limit 15
     @produtosPorPreco = Produto.order(:preco).limit(2)
     @liv = Livro.order :titulo
+  end
+
+  def create
+    valores = params.require(:produto).permit :nome, :descricao, :quantidade, :preco # Pega o params faz uma requisição do produtos e permite todos
+    produto = Produto.create valores
+
   end
 
 end
